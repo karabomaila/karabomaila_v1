2fba21a6bd934511bad71122983c62df98cbe891
karabomaila <mailakarabo3@gmail.com>
2022-11-02 22:30:55 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route, Routes} from 'react-router-dom';
-import {BrowserRouter} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Contact from './components/Contact'
