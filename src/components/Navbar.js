256d9f7c52a2e727354793c008ed2de20fb50c75
karabomaila <mailakarabo3@gmail.com>
2022-11-05 19:06:09 +0200

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="navbar">
             <a href="/" className="logo">Karabo Maila</a>
             <ul>
-                <li><NavLink className="link" to="/">Home</NavLink></li>
+                <li className="active"><NavLink className="link" to="/">Home</NavLink></li>
                 <li><NavLink className="link" to="/blogs">Blogs</NavLink></li>
                 <li><NavLink className="link" to="/photography">Photography</NavLink></li>
                 <li><NavLink className="link" to="/contact">Contact</NavLink></li>
